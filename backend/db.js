5497331a2198dbaf8c88e17ca4f904875c38d8da
Rhoda Shiny <your-email@gmail.com>
2026-05-01 05:59:56 +0530

    switch db to aiven

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,        // Railway host
-  user: process.env.DB_USER,        // root
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,    // railway
-  port: process.env.DB_PORT,        // 24870
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false
   }
@@ -15,7 +15,7 @@ db.connect((err) => {
   if (err) {
     console.error("❌ Database connection failed:", err);
   } else {
-    console.log("✅ Connected to Railway MySQL");
+    console.log("✅ Connected to Aiven MySQL Database");
   }
 });
 
